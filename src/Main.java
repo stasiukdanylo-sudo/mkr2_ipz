7e3ba2b685560e83cfa46561175cac00b9ceb704
StasiukDanylo <stasiuk.danylo@lll.kpi.ua>
2025-12-12 17:52:24 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,17 @@ public class Main {
 
         EventManager eventManager = EventManager.getInstance();
 
-        // створення модулів
         AuthModule auth = new AuthModule();
         LoggingModule logger = new LoggingModule();
         UserInterfaceModule ui = new UserInterfaceModule();
 
-        // підписуємо їх на менеджер подій
         eventManager.subscribe(auth);
         eventManager.subscribe(logger);
         eventManager.subscribe(ui);
 
-        // генеруємо події
         auth.userLogin("Danylo");
         auth.userLogout("Danylo");
 
-        // продемонструємо, що Singleton працює
         System.out.println("\nEventManager instance check: " +
                 (eventManager == EventManager.getInstance()));
     }
